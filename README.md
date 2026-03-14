e52e0ee20e1495d04e489879d9160759065ccd53
egjarabo <e.glez.telefonica@gmail.com>
2026-03-14 19:48:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-ecommerce-api
+REST API for e-commerce platform built with Spring Boot 3, Java 21, JPA, PostgreSQL and Testcontainers
---
+++ src/test/java/com/egjarabo/ecommerce/SpringBootEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egjarabo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
